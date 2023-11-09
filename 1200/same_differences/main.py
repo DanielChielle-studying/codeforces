56c0e1cf1a824a61a0aa234c9f6f60580649f162
DanielChielle-studying <danielchielle.studying@gmail.com>
2023-11-09 10:47:05 -0300

    Codeforces exercises
    Same Differences - 1200

---
+++ 1200/same_differences/main.py
@@ -0,0 +1,25 @@
+def main():
+    t = int(input())
+
+    for _ in range(t):
+        n = int(input())
+        a = list(map(int, input().split()))
+
+        difference = {}
+        pairs = 0
+
+        for i in range(n):
+            diff = a[i] - i
+
+            if diff in difference:
+                pairs += difference[diff]
+
+            if diff in difference:
+                difference[diff] += 1
+            else:
+                difference[diff] = 1
+
+        print(pairs)
+
+if __name__ == "__main__":
+    main()
