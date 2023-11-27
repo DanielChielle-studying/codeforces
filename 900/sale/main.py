909cc9d1261143982f6ea6fe9f741600f3b0139f
DanielChielle-studying <danielchielle.studying@gmail.com>
2023-11-27 20:11:15 -0300

    Codeforces update
    Improving code:
    Sale - 900

--- 900/sale/main.py
+++ 900/sale/main.py
@@ -1,12 +1,16 @@
-n, m = map(int, input().split())
-tvs = sorted(list(map(int, input().split())))
+def main():
+    n, m = map(int, input().split())
+    tvs = sorted(list(map(int, input().split())))
 
-sum_momey = 0
+    sum_momey = 0
 
-for i in range(m):
-    if tvs[i] <= 0:
-        sum_momey += abs(tvs[i])
-    else:
-        break
+    for i in range(m):
+        if tvs[i] <= 0:
+            sum_momey += tvs[i]
+        else:
+            break
 
-print(sum_momey)
+    print(-1*sum_momey)
+
+if __name__ == "__main__":
+    main()
