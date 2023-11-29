e1bc2623ada5d1b444f37babe6e90ab3b741029c
DanielChielle-studying <danielchielle.studying@gmail.com>
2023-11-28 21:10:09 -0300

    Codeforces update
    Fear of the dark - 1200
     - Code improvement: runs 30ms faster

--- 1200/fear_of_the_dark/main.py
+++ 1200/fear_of_the_dark/main.py
@@ -1,7 +1,4 @@
-from math import sqrt
-
-def get_distance(ax, ay, bx = 0, by = 0):
-	return sqrt((bx - ax)**2 + (by - ay)**2)
+from math import dist
 
 def get_mid_point(ax, ay, bx, by):
 	return (ax + bx) / 2, (ay + by) / 2
@@ -15,23 +12,28 @@ def main():
 		ax, ay = map(int, input().split())
 		bx, by = map(int, input().split())
 
-		distance_a_p = get_distance(ax, ay, px, py)
-		distance_b_p = get_distance(bx, by, px, py)
+		O = [0, 0]
+		P = [px, py]
+		A = [ax, ay]
+		B = [bx, by]
 
-		distance_a_o = get_distance(ax, ay)
-		distance_b_o = get_distance(bx, by)
+		distance_a_p = dist(A, P)
+		distance_a_o = dist(A, O)
+
+		distance_b_p = dist(B, P)
+		distance_b_o = dist(B, O)
 
 		closer_p = (ax, ay) if distance_a_p <= distance_b_p else (bx, by)
 		closer_o = (ax, ay) if distance_a_o <= distance_b_o else (bx, by)
 
-		distance_closer_p = get_distance(closer_p[0], closer_p[1], px, py)
-		distance_closer_o = get_distance(closer_o[0], closer_o[1])
+		distance_closer_p = dist(closer_p, P)
+		distance_closer_o = dist(closer_o, O)
 
 		if closer_p == closer_o:
 			print(max(distance_closer_p, distance_closer_o))
 		else:
 			mid_point = get_mid_point(ax, ay, bx, by)
-			distance_mid = get_distance(ax, ay, mid_point[0], mid_point[1])
+			distance_mid = dist(A, mid_point)
 			print(max(distance_mid, distance_closer_p, distance_closer_o))
 
 if __name__ == '__main__':
